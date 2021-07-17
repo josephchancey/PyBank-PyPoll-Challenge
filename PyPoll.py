dbc0484ef3fd0f8c4ba78a416e9d8d8a59926b67
Joseph Chancey <84075822+josephchancey@users.noreply.github.com>
2021-07-17 15:05:28 -0700

    Add loop to identify headers
    Loop through dataset and grab first row for headers.

--- PyPoll.py
+++ PyPoll.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[5]:
 
 
 # Python 3 PyPoll Challenge Script 
@@ -16,4 +16,29 @@ import csv
 # Load File + Define Output path, assign to variables
 poll_csv = os.path.join(".", "Resources", "election_data.csv")
 output_file = os.path.join(".", "Resources", "PyPoll_Election_Analysis.txt")
+
+# Totaling
+total_votes = 0
+
+# Candidates
+candidate_options = []
+candidate_votes = { }
+
+# Winning Count Container
+winning_candidate = ""
+winning_total = 0
+
+# Loop Through and Read Dataset
+with open(poll_csv) as election_data:
+    reader = csv.reader(election_data)
+    
+    # Identify header through 'next()' function
+    header = next(reader)
+    print(header)
+
+
+# In[ ]:
+
+
+
 

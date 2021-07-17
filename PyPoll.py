e2a977ec3ee64436d38a731f356f857c1fdad4f2
Joseph Chancey <84075822+josephchancey@users.noreply.github.com>
2021-07-17 15:36:43 -0700

    Add .txt output after vote count

--- PyPoll.py
+++ PyPoll.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[4]:
 
 
 # Python 3 PyPoll Challenge Script 
@@ -37,8 +37,8 @@ with open(poll_csv) as election_data:
     
     # Loader animation
     for i in reader:
-        # Loading Dots
-        print(". ", end="")
+        # Loading Dots - Commented Out Due To Performence Issues
+        #print(". ", end="")
         
         # Total Vote Count Adder
         total_votes = total_votes + 1
@@ -54,7 +54,44 @@ with open(poll_csv) as election_data:
             # Count Candidate Votes
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
-            
+        
+# Output/Print results
+with open(output_file, "w") as txt_file:
+    election_results = (f"\nElection Results\n"
+                       f"-------------------------\n"
+                       f"Total Votes: {total_votes}\n"
+                       f"-------------------------\n")
+    
+    #Print results
+    print(election_results, end="")
+    
+    # Write Summary .txt File
+    txt_file.write(election_results)
+    
+    # Vote Count For Each Candidate And Percentage of Total Votes
+    for candidate in candidate_votes:
+        votes = candidate_votes.get(candidate)
+        vote_percent = float(votes) / float(total_votes) * 100
+        
+        # Pick Winning Vote Count
+        if(votes > winning_total):
+            winning_total = votes
+            winning_candidate = candidate
+        
+        voter_output = f"{candidate}: {vote_percent:.3f}% ({votes})\n"
+
+        print(voter_output, end="")
+
+        txt_file.write(voter_output)
+        
+    # Statement For Winning Candidate
+    winning_candidate_summary = (f"-------------------------\n"
+                                f"Winner: {winning_candidate}\n"
+                                f"-------------------------\n")
+    
+    # Update and Add Winning Candidate
+    print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
 
 
 # In[ ]:
